0d7835de600d4bd96dbeead0635416e020402dde
komorebi920 <yxc900920@163.com>
2023-07-17 16:27:59 +0800

    feat: 限制上传文件类型（仅支持图片），调整返回数据，上传成功后展示图片，兼容 ie11

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { koaBody } = require("koa-body");
 
 // 指定了静态文件的根目录
 app.use(static(__dirname + "/public"));
+app.use(static(__dirname + "/uploads"));
 
 // 使用 koa-body 中间件来处理文件上传
 app.use(
@@ -22,16 +23,16 @@ app.use(
 // 设置路由处理函数
 router.post("/upload", async (ctx) => {
   const file = ctx.request.files.file; // 获取上传的文件对象
-  const filePath = file.filepath; // 获取文件的临时路径
-  const newFilename = file.newFilename; // 获取文件的临时名称
-  const originalFilename = file.originalFilename; // 获取文件的原始名称
 
-  console.log("filePath: ", filePath);
-  console.log("originalFilename: ", originalFilename);
-  console.log("newFilename: ", newFilename);
-  // 其他处理逻辑...
+  console.log("filePath: ", file.filepath); // 上传文件的新路径
+  console.log("originalFilename: ", file.newFilename); // 上传文件的新名称
+  console.log("newFilename: ", file.originalFilename); // 上传文件的原始名称
 
-  ctx.body = { code: 0, message: "文件上传成功" };
+  ctx.body = {
+    code: 0,
+    message: "文件上传成功",
+    data: { url: file.newFilename },
+  };
 });
 
 // 将路由注册到应用
